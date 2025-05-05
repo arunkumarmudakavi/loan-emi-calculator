411bd6cd3b2ffcae57028e4471e85c0b6675c662
arunkumarmudakavi <arunkumarmudakavi029@gmail.com>
2025-05-05 19:12:12 +0530

    Wrapped everything with theme provider and routes are implemented

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,31 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { About, ExchangeRates } from "./components/index.js"; 
+import { ThemeContextProvider } from "./context/ThemeContextProvider.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/exchange-rates",
+        element: <ExchangeRates />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ThemeContextProvider>
+      <RouterProvider router={router}/>
+    </ThemeContextProvider>
+  </StrictMode>
+);
