1588039098634590e7bf3254ea224d00e6783fd3
arunkumarmudakavi <arunkumarmudakavi029@gmail.com>
2025-05-06 18:57:32 +0530

    Customized error component

--- src/utils/Error.jsx
+++ src/utils/Error.jsx
@@ -1,7 +1,17 @@
-const Error = () => {
-  return (
-    <>404</>
-  )
-}
+import { Box, Button, Typography, useTheme } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
-export {Error}
+const Error = () => {
+  const navigate = useNavigate();
+  const theme = useTheme();
+  return (
+    <Box sx={{height: "95dvh", display: "flex",flexDirection:"column", justifyContent:"center", alignItems:"center"}}>
+      <Typography variant="h4" sx={{color: theme.palette.primary.main ,mb:2 }}>Something Went Wrong!!!</Typography>
+      <Button variant="outlined" onClick={() => navigate("/")}>
+        GO HOME
+      </Button>
+    </Box>
+  );
+};
+
+export { Error };
